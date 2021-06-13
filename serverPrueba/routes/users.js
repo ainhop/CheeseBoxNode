cccd9869e943899425639ab1909fb2bca921bba9
ainhoa <ainhoapalacio>
2021-06-13 14:31:41 +0200

    + pruebas

--- serverPrueba/routes/users.js
+++ serverPrueba/routes/users.js
@@ -7,3 +7,13 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+// var express = require("express");
+// var router = express.Router();
+
+// /* GET home page. */
+// router.get("/Usuarios", function (req, res, next) {
+//   res.render("Usuarios");
+// });
+
+// module.exports = router;
