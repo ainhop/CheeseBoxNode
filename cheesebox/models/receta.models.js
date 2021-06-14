3fefb44cf349e7d1ec594d4b5a4de286272bd4ac
ainhoa <ainhoapalacio>
2021-06-14 19:25:42 +0200

    consultas recetas

--- cheesebox/models/receta.models.js
+++ cheesebox/models/receta.models.js
@@ -3,10 +3,80 @@
 // obtener todas las recetas
 const getAll = () => {
   return new Promise((resolve, reject) => {
-    db.query("select * from recetas"),
-      (err, rows) => {
-        if (err) reject(err);
-        resolve(rows);
-      };
+    db.query("select * from recetas", (err, rows) => {
+      if (err) reject(err);
+      resolve(rows);
+    });
   });
+};
+
+const create = ({
+  nombre,
+  quesoUtilizado,
+  ingredientes,
+  tiempo,
+  raciones,
+  elaboracion,
+}) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      "insert into recetas (nombre, quesoUtilizado, ingredientes, tiempo, raciones, elaboracion) values (?, ?, ?, ?, ?, ?)",
+      [nombre, quesoUtilizado, ingredientes, tiempo, raciones, elaboracion],
+      (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      }
+    );
+  });
+};
+
+const getById = (pId) => {
+  return new Promise((resolve, reject) => {
+    db.query("select * from recetas where id = ?", [pId], (err, rows) => {
+      if (err) reject(err);
+      if (rows.length !== 1) resolve(null);
+      resolve(rows[0]);
+    });
+  });
+};
+
+const update = (
+  pRecetaId,
+  { nombre, quesoUtilizado, ingredientes, tiempo, raciones, elaboracion }
+) => {
+  return new Promise((resolve, reject) => {
+    db.query(
+      "UPDATE recetas SET nombre = ?, quesoUtilizado = ?, ingredientes = ?, tiempo = ?, raciones = ?, elaboracion = ? WHERE id = ?",
+      [
+        nombre,
+        quesoUtilizado,
+        ingredientes,
+        tiempo,
+        raciones,
+        elaboracion,
+        pRecetaId,
+      ],
+      (err, result) => {
+        if (err) reject(err);
+        resolve(result);
+      }
+    );
+  });
+};
+
+const deleteById = (pRecetaId) => {
+  return new Promise((resolve, reject) => {
+    db.query("delete from recetas where id = ?", [pRecetaId], (err, result) => {
+      if (err) reject(err);
+      resolve(result);
+    });
+  });
+};
+
+module.exports = {
+  getAll,
+  create,
+  getById,
+  update,
+  deleteById,
 };
--- cheesebox/routes/recetas.js
+++ cheesebox/routes/recetas.js
@@ -1,9 +1,58 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", function (req, res) {
-  res.json("Todas las recetas");
+const {
+  getAll,
+  create,
+  getById,
+  update,
+  deleteById,
+} = require("../models/receta.models");
+
+// GET http://localhost:3000/recetas
+router.get("/", async (req, res) => {
+  try {
+    const recetas = await getAll();
+    res.json(recetas);
+  } catch (error) {
+    console.log("David: se ha roto esto");
+  }
+});
+// POST http://localhost:3000/recetas/create
+router.post("/create", async (req, res) => {
+  try {
+    const result = await create(req.body);
+    res.json(result);
+  } catch (error) {
+    ("David dice super error");
+  }
+});
+
+router.get("/:recetaId", async (req, res) => {
+  try {
+    const receta = await getById(req.params.recetaId);
+    if (receta) {
+      res.json(receta);
+    } else {
+      res.json({ message: "El id no existe" });
+    }
+  } catch (error) {
+    res.json({ error: "no funciona" });
+  }
+});
+
+router.put("/update/:recetasId", async (req, res) => {
+  try {
+    const result = await update(req.params.recetasId, req.body);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.delete("/delete/:recetasId", async (req, res) => {
+  const result = await deleteById(req.params.recetasId);
+  res.json(result);
 });
 
 module.exports = router;
