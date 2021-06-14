d6883490de4dc82e776b0945b29077b6486a4b66
ainhoa <ainhoapalacio>
2021-06-14 13:00:19 +0200

    res.jsons

--- cheesebox/routes/usuarios.js
+++ cheesebox/routes/usuarios.js
@@ -4,9 +4,8 @@ var router = express.Router();
 const usuariosProductosRouter = require("./usuarios/productos");
 const usuariosRecetasRouter = require("./usuarios/recetas");
 
- router.get("/", function (req, res, next) {
-   res.render("Usuarios");
- });
+router.get("/", function (req, res, next) {
+  res.json("Usuarios");
+});
 
- module.exports = router;
-
+module.exports = router;
