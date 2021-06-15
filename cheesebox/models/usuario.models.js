24b77d25012a6536c45c418a3b09fa039ad63313
David <daviddelpozocalderon@gmail.com>
2021-06-15 13:36:00 +0200

    Reparación peticiones
    Usuarios.

--- cheesebox/models/usuario.models.js
+++ cheesebox/models/usuario.models.js
@@ -2,13 +2,22 @@ const create = ({
   nombre, apellidos, username, email, password
 }) => {
   return new Promise((resolve, reject) => {
-      db.query('insert into productos (nombre, apellidos, username, email, password) values (?, ?, ?, ?, ?)', [ nombre, apellidos, username, email, password], (err, result) => {
+      db.query('insert into usuarios (nombre, apellidos, username, email, password) values (?, ?, ?, ?, ?)', [ nombre, apellidos, username, email, password], (err, result) => {
           if (err) reject(err);
           resolve(result);
       });
   });
 }
 
+const getById = (pUsuarioId) => {
+  return new Promise((resolve, reject) => {
+      db.query('select * from usuarios where id = ?', [pUsuarioId], (err, rows) => {
+          if (err) reject(err);
+          // if (rows.length !== 1) resolve(null);
+          resolve(rows[0]);
+      })
+  });
+}
 
 
 const deleteById = (pUsuarioId) => {
@@ -24,17 +33,18 @@ const deleteById = (pUsuarioId) => {
   
 }
 const update = (pUsuarioId, { nombre, apellidos, username, email, password }) => {
-    
+   console.log('Algo me da igual') 
   return new Promise((resolve, reject) => {
       db.query(
           'UPDATE usuarios SET nombre = ?, apellidos = ?, username = ?, email = ?, password = ? where id = ?',
           [nombre, apellidos, username, email, password, pUsuarioId],
           (err, result) => {
-              if (err) reject(err);
+            if (err) reject(err);
+            console.log(result)
               resolve(result);
           });
   });
 }
 
 
-module.exports = { create, deleteById, update };
+module.exports = { create, getById, deleteById, update };
