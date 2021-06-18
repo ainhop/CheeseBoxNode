e1ac211d91087b1c44e6b3c5cc9318af796a30af
ainhoa <ainhoapalacio>
2021-06-18 08:44:58 +0200

    middlewares

--- cheesebox/routes/productos.js
+++ cheesebox/routes/productos.js
@@ -1,83 +1,72 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const { getAll, create, getById, update, deleteById } = require('../models/producto.models');
-const path = require('path');
-const fs = require('fs')
-const multer = require('multer');
-const upload = multer({ dest: 'public/images/productos/' });
+const {
+  getAll,
+  create,
+  getById,
+  update,
+  deleteById,
+} = require("../models/producto.models");
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+const upload = multer({ dest: "public/images/productos/" });
 const app = express();
 
 app.use(express.json);
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
-
-
-const path = require('path');
-const fs = require('fs')
-const multer = require('multer');
-const upload = multer({ dest: 'public/images/productos/' });
-const app = express();
-
-app.use(express.json);
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 router.get("/", async (req, res) => {
-  try{
+  try {
     const productos = await getAll();
-    res.json(productos)
-  } 
-  catch (error) {
-    (console.error('Sigue buscando'))
+    res.json(productos);
+  } catch (error) {
+    console.error("Sigue buscando");
   }
 });
 
-router.post('/create', upload.single('imagen'), async (req, res) => {
+router.post("/create", upload.single("imagen"), async (req, res) => {
   try {
-    const extension = '.' + req.file.mimetype.split('/')[1];
-    const newName = 'http://localhost:3000/images/productos/' + req.file.filename + extension;
+    const extension = "." + req.file.mimetype.split("/")[1];
+    const newName =
+      "http://localhost:3000/images/productos/" + req.file.filename + extension;
     const newPath = req.file.path + extension;
-    fs.renameSync(req.file.path, newPath )
-    req.body.imagen = newName
+    fs.renameSync(req.file.path, newPath);
+    req.body.imagen = newName;
     const result = await create(req.body);
-      res.json(result);
+    res.json(result);
   } catch (error) {
-      console.log(error);
+    console.log(error);
   }
-})
+});
 
-
-
-router.get('/:productoId', async (req, res) => {
+router.get("/:productoId", async (req, res) => {
   try {
     const producto = await getById(req.params.productoId);
     if (producto) {
-      res.json(producto)
- 
+      res.json(producto);
     } else {
-      ('este ID es mentira')
+      ("este ID es mentira");
     }
+  } catch (error) {
+    res.json("Error mu chungo");
   }
-  catch(error) {
-    res.json('Error mu chungo')
-  }
-})
+});
 
-router.delete('/delete/:productosId', async (req, res) => {
+router.delete("/delete/:productosId", async (req, res) => {
   const result = await deleteById(req.params.productosId);
   res.json(result);
-})
+});
 
-router.put('/update/:productosId', async (req, res) => {
- try{
-  const result = await update(req.params.productosId, req.body);
-   res.json(result);
+router.put("/update/:productosId", async (req, res) => {
+  try {
+    const result = await update(req.params.productosId, req.body);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
   }
- catch (error) {
-  console.log(error)
-  }
-})
+});
 module.exports = router;
 // app.listen(3300, () => {
 //   console.log('El servidor esta escuchando en el puerto 3000')
--- cheesebox/routes/usuarios.js
+++ cheesebox/routes/usuarios.js
@@ -1,39 +1,74 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+
+const bcrypt = require("bcryptjs");
+const dayjs = require("dayjs");
+const jwt = require("jsonwebtoken");
 
 const {
   create,
   deleteById,
   update,
   getById,
+  getByEmail,
 } = require("../models/usuario.models");
 
-const path = require('path');
-const fs = require('fs')
-const multer = require('multer');
-const upload = multer({ dest: 'public/images/usuarios/' });
-const app = express();
+const path = require("path");
+const fs = require("fs");
+const multer = require("multer");
+const { checkToken } = require("../middlewares/middleware");
+const upload = multer({ dest: "public/images/usuarios/" });
 
-app.use(express.json);
-app.use(express.urlencoded({ extended: false }))
-app.use(express.static(path.join(__dirname, 'public')))
-
-
-router.post('/create', upload.single('imagen'), async (req, res) => {
-  try {
-    const extension = '.' + req.file.mimetype.split('/')[1];
-    const newName = 'http://localhost:3000/images/usuarios/' + req.file.filename + extension;
-    const newPath = req.file.path + extension;
-    fs.renameSync(req.file.path, newPath )
-    req.body.imagen = newName
-    const result = await create(req.body);
-      res.json(result);
-  } catch (error) {
-      console.log(error);
+function validate(body) {
+  if (body.nombre === "") {
+    return {
+      error: "The name is empty",
+    };
   }
-})
 
+  return { ...body };
+}
 
+router.get("/test", checkToken, (req, res) => {
+  console.log("el token es válido");
+
+  res.json("ok");
+});
+
+router.post("/create", upload.single("imagen"), async (req, res) => {
+  try {
+    const extension = "." + req.file.mimetype.split("/")[1];
+    const newName =
+      "http://localhost:3000/images/usuarios/" + req.file.filename + extension;
+    const newPath = req.file.path + extension;
+    fs.renameSync(req.file.path, newPath);
+    req.body.imagen = newName;
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: erros.array() });
+    }
+
+    const body = validate(req.body);
+
+    if (body.error) {
+      return res.status(400).json(body.error);
+    }
+
+    const usuario = await getByEmail(req.body.email);
+
+    if (usuario) {
+      return res.status(400).json({ error: "el email está registrado" });
+    }
+
+    req.body.password = bcrypt.hashSync(req.body.password, 8);
+
+    const result = await create(req.body);
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 router.put("/update/:usuarioId", async (req, res) => {
   try {
@@ -42,8 +77,7 @@ router.put("/update/:usuarioId", async (req, res) => {
   } catch (error) {
     console.log(error);
   }
-})
-
+});
 
 router.get("/:usuarioId", async (req, res) => {
   try {
@@ -76,5 +110,38 @@ router.post("/registro", (req, res) => {
       console.log(error);
     });
 });
+
+router.post("/login", async (req, res) => {
+  const emailValid = req.body.email.match(
+    /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+  );
+  if (!emailValid) {
+    return res.status(400).json({ error: "el email no es válido" });
+  }
+
+  const usuario = await getByEmail(req.body.email);
+  if (!usuario) {
+    return res.json({ error: "error en email o password" });
+  }
+  const same = bcrypt.compareSync(req.body.password, usuario.password);
+
+  if (same) {
+    return res.json({ success: "correcto", token: createToken(usuario) });
+  }
+  res.json({ error: "error en email o password" });
+});
+
+function createToken(usuario) {
+  // retorna en token
+
+  const obj = {
+    usuario: {
+      id: usuario.id,
+      email: usuario.email,
+    },
+    caducidad: dayjs().add(7, "minutes").unix(),
+  };
+  return jwt.sign(obj, "un string");
+}
 
 module.exports = router;
