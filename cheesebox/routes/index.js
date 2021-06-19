0af6a6933e7e54bb5e8efaae861cd84f17bc71c8
David <daviddelpozocalderon@gmail.com>
2021-06-19 18:32:34 +0200

    reparacion consultas

--- cheesebox/routes/index.js
+++ cheesebox/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const { getByItem } = require('../models/index.models');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Bienvenido al index');
+  res.send('');
 });
 
+// router.get("/:producto", async (req, res) => {
+//   try {
+//     const producto = await getByItem(req.params.producto);
+//     if (producto) {
+//       res.json(producto)
+ 
+//     } else {
+//       ('este queso no está')
+//     }
+//   }
+//   catch(error) {
+//     res.json('Ups algo no fue bien')
+//   }
+// });
 module.exports = router;
